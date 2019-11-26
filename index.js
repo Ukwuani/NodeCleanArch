d1c78617daecd5070d8982834e0245e12af944a0
ukwuani <Chimbudike1>
2019-11-26 12:08:28 +0000

    testhint

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-`use strict`
-const express = require(express)
-const app = express()
-const port = 3000
+const app = require(express)();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
